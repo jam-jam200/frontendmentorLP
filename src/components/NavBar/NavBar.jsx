9db4f2b0619fa185cc59ff86cd5d5916e8c149cc
jam-jam200 <jessieoyaks4j@gmail.com>
2022-11-28 13:34:10 +0100

    nav

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import { Logo, ArrowDown, ArrowUp, Close, Menubar } from "../../assets";
+
+const Menu = () => {
+  return (
+    <>
+      <p>
+        <a href="/">Features</a>
+      </p>
+      <p>
+        <a href="/">Company</a>
+      </p>
+      <p>
+        <a href="/">Careers</a>
+      </p>
+      <p>
+        <a href="/">About</a>
+      </p>
+    </>
+  );
+};
+
+const NavBar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <div className="snap__navbar">
+      <div className="snap__navbar-links">
+        <div className="snap__navbar-links_logo">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="snap__navbar-links_container">
+          <Menu className="snap__navbar-links_container" />
+        </div>
+      </div>
+      <div className="snap__navbar-sign">
+        <p>Login</p>
+        <button type="button">Register</button>
+      </div>
+      <div className="snap__navbar-menu">
+        {toggleMenu ? (
+          <img
+            src={Close}
+            alt="close button"
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <img
+            src={Menubar}
+            alt="menubar"
+            onClick={() => setToggleMenu(false)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="snap__navbar-menu_container scale-up-center">
+            <div className="snap__navbar-menu_container-links">
+              <Menu />
+              <div className="snap__navbar-menu_container-links-sign">
+                <p>Login</p>
+                <button type="button">Register</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
